22e0e32e7fd70030a84d4971442732cdb873f4a0
Cre8tion <ljl1997@gmail.com>
2020-03-25 18:19:04 +0800

    Added more UI elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Connection from './components/connection';
-import {Form} from 'react-bootstrap'
+import {Form, Row, Col, Button} from 'react-bootstrap'
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
+
+const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
 
 
 const { BN, units } = require('@zilliqa-js/util');
@@ -11,32 +14,39 @@ const { toBech32Address } = require('@zilliqa-js/crypto');
 function App() {
   const [state, setState] = useState({});
 
+  useEffect(() => {
+
+    async function fetchdata(){
+      const deployed = zilliqa.contracts.at("0x1843899294586A48C3DA9518037b3aBc8f44eD32");
+      const state = await deployed.getState();
+      console.log(state);
+      console.log(JSON.stringify(state, null, 4));
+    }
+
+    fetchdata();
+
+  })
 
   return (
     <div className="App">
         <div className="col-lg-12">
-        <h1 className="text-center">Election Results</h1>
-
-        
-        <Form>
-          <Form.Group controlId="exampleForm.ControlSelect1">
-            <Form.Label>Select Candidate</Form.Label>
-            <Form.Control as="select">
-              <option>Candidate 1</option>
-              <option>Candidate 2</option>
-            </Form.Control>
-          </Form.Group>
-        </Form>
-        <form onSubmit="">
-              <div class="form-group">
-                <label for="candidatesSelect">Select Candidate</label>
-                <select class="form-control" id="candidatesSelect">
-                </select>
-              </div>
-              <button type="submit" class="btn btn-primary">Vote</button>
-              <hr />
-        </form>
+          <h1 className="text-center">Election Results</h1>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Select Candidate</Form.Label>
+              <Form.Control as="select">
+                <option>Candidate 1</option>
+                <option>Candidate 2</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Col>
+                <Button type="submit">Vote</Button>
+              </Col>
+            </Form.Group>
+          </Form>
         </div>
+
         <Connection />
     </div>
   );
