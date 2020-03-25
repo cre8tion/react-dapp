d3ae11b2867fc405ef369b0d02a2095e0cf89177
Cre8tion <ljl1997@gmail.com>
2020-03-25 17:32:35 +0800

    Added form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Connection from './components/connection';
+import {Form} from 'react-bootstrap'
 
 
 const { BN, units } = require('@zilliqa-js/util');
@@ -17,6 +18,24 @@ function App() {
         <h1 className="text-center">Election Results</h1>
 
         
+        <Form>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Select Candidate</Form.Label>
+            <Form.Control as="select">
+              <option>Candidate 1</option>
+              <option>Candidate 2</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <form onSubmit="">
+              <div class="form-group">
+                <label for="candidatesSelect">Select Candidate</label>
+                <select class="form-control" id="candidatesSelect">
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary">Vote</button>
+              <hr />
+        </form>
         </div>
         <Connection />
     </div>
