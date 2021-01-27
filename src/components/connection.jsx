ca6951d6e278abe3c075208fe4032f4d1623288f
Cre8tion <ljl1997@gmail.com>
2021-01-27 17:46:50 +0800

    Updated Dependancies

--- src/components/connection.jsx
+++ src/components/connection.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Connection = ({ children }) => {
   const [connected, setConnected] = useState(false);
+  const [status, setStatus] = useState("Not connected")
 
   useEffect(() => {
     let didCancel = false;
@@ -21,10 +22,19 @@ const Connection = ({ children }) => {
     <React.Fragment>
       {!connected && (
         <React.Fragment>
-          <p>Not connected</p>
+          <p>{status}</p>
           <button onClick={async () => {
-            const isConnect = await window.zilPay.wallet.connect();
-            setConnected(isConnect);
+            try{
+              if (window.zilPay !== undefined){
+                const isConnect = await window.zilPay.wallet.connect();
+                setConnected(isConnect);
+              }
+              else{
+                setStatus("Please install Zilpay Wallet on Browser")
+              }
+            } catch(e){
+                setStatus("Unexpected error occurred")
+              }
             }}>Connect Wallet</button>
         </React.Fragment>
       )}
